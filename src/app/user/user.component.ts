5950e59f120fb038157ea31f26abc0fec13b7158
Marco <marco.beer93@web.de>
2022-07-04 16:17:46 +0200

    added logo and the user array now appears on the userscreen when typed in

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Firestore } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
@@ -12,13 +14,22 @@ export class UserComponent implements OnInit {
 
   user = new User();
 
+  allUsers: any = [];
 
-  constructor(public dialog: MatDialog) {
+
+  constructor(public dialog: MatDialog, private firestore: AngularFirestore) {
 
 
   }
 
   ngOnInit(): void {
+    this.firestore
+      .collection('users')
+      .valueChanges()
+      .subscribe((changes: any) => {
+        console.log('recieved changes from database');
+        this.allUsers = changes;
+      });
   }
 
   openDialog() {
